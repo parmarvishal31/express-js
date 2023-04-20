cc479b5ab1149a66ac5bb72d95c18f42f0656dc9
vishalparmar31 <vishalparmar31320@gmail.com>
2023-04-20 11:46:27 +0530

    post method ande middleware espress josn , bodyParser ,morgan

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 // import 
 const express = require('express')
 const colors = require('colors');
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
 // rest object means express feture copy in app variable
 const app = express()
+
+// middlelware
+
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(morgan('dev'))
 
 // routes
 
 app.get("/", (req, res) => {
     res.send("<h1>Learn Express by vishal</h1>")
+})
+
+app.post("/contect", (req, res) => {
+    const { name } = req.body
+    res.json({
+        success: true,
+        message: `Welcome ${name}`
+    })
 })
 
 // port
